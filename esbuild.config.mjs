ccf6c591ffb3c4a57950792da72b5df5a2f46acc
Johann Richard <189003+johannrichard@users.noreply.github.com>
2025-10-03 08:50:14 +0200

    build: 👷 update build system for src reorg

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -6,48 +6,48 @@ import copy from "esbuild-copy-static-files";
 const prod = process.argv[2] === "production";
 
 const context = await esbuild.context({
-  entryPoints: ["main.ts"],
-  bundle: true,
-  minify: prod,
-  external: [
-    "obsidian",
-    "electron",
-    "@codemirror/autocomplete",
-    "@codemirror/collab",
-    "@codemirror/commands",
-    "@codemirror/language",
-    "@codemirror/lint",
-    "@codemirror/search",
-    "@codemirror/state",
-    "@codemirror/view",
-    "@lezer/common",
-    "@lezer/highlight",
-    "@lezer/lr",
-    ...builtins,
-  ],
-  format: "cjs",
-  target: "es2018",
-  logLevel: "info",
-  sourcemap: prod ? false : "inline",
-  treeShaking: true,
-  outfile: "build/main.js",
-  plugins: [
-    copy({
-      src: "manifest.json",
-      dest: "build/manifest.json",
-      watch: !prod,
-    }),
-    copy({
-      src: "styles.css",
-      dest: "build/styles.css",
-      watch: !prod,
-    }),
-  ],
+	entryPoints: ["src/main.ts"],
+	bundle: true,
+	minify: prod,
+	external: [
+		"obsidian",
+		"electron",
+		"@codemirror/autocomplete",
+		"@codemirror/collab",
+		"@codemirror/commands",
+		"@codemirror/language",
+		"@codemirror/lint",
+		"@codemirror/search",
+		"@codemirror/state",
+		"@codemirror/view",
+		"@lezer/common",
+		"@lezer/highlight",
+		"@lezer/lr",
+		...builtins,
+	],
+	format: "cjs",
+	target: "es2018",
+	logLevel: "info",
+	sourcemap: prod ? false : "inline",
+	treeShaking: true,
+	outfile: "build/main.js",
+	plugins: [
+		copy({
+			src: "manifest.json",
+			dest: "build/manifest.json",
+			watch: !prod,
+		}),
+		copy({
+			src: "styles.css",
+			dest: "build/styles.css",
+			watch: !prod,
+		}),
+	],
 });
 
 if (prod) {
-  await context.rebuild();
-  process.exit(0);
+	await context.rebuild();
+	process.exit(0);
 } else {
-  await context.watch();
+	await context.watch();
 }
